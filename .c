8e34f138a6be3bd823b3f4617af86deb475c511b
Guiggass <guicavalar@gmail.com>
2026-08-14 20:06:28 -0300

    Create .c

---
+++ .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+	printf("Mackenzie - FCI");
+	printf(" Algoritmo e");
+	printf(" Programação II\n");
+	printf(" Turma -N11\n");
+	system("pause");
+
+	return 0;
+}
